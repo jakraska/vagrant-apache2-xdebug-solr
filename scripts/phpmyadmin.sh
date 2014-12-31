3d19781df689080a365674a5b37a56b650a56e8f
Jonathan Kraska <jonathan@avatarnewyork.com>
2014-12-31 00:15:14 -0500

    fixed auto-linking for phpmyadmin install

--- scripts/phpmyadmin.sh
+++ scripts/phpmyadmin.sh
@@ -4,5 +4,7 @@
 
 # Install phpMyAdmin
 echo "[interactive install] Installing phpMyAdmin..."
-sudo apt-get install -y phpmyadmin 
+sudo apt-get install -y phpmyadmin
+cd /etc/apache2/sites-enabled
+sudo ln -s ../../phpmyadmin/apache.conf phpmyadmin.conf
 sudo service apache2 restart # restart apache so latest php config is picked up
